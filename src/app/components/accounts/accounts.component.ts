fef08dd5e7d2d3b499682a5de5b6aa27ce93f418
Othman Nassik <othmannassik1@gmail.com>
2023-06-02 01:27:13 +0100

    Add Debit, Credit & Transfert Operations

--- src/app/components/accounts/accounts.component.ts
+++ src/app/components/accounts/accounts.component.ts
@@ -14,21 +14,61 @@ export class AccountsComponent implements OnInit{
   cuurentPage : number = 0;
   pageSize : number = 5;
   account$! : Observable<AccountDetail>;
+  operationFormGroup! : FormGroup;
   constructor(private fb : FormBuilder, private accountService : AccountsService) {
   }
   ngOnInit(): void {
     this.accountFormGroup = this.fb.group({
       accountId : this.fb.control("")
     });
+    this.operationFormGroup = this.fb.group({
+      operationType : this.fb.control(""),
+      amount : this.fb.control(0),
+      description : this.fb.control(""),
+      accountDestination : this.fb.control("")
+    });
   }
 
   handleSearchAccount() {
-    let accountId : String = this.accountFormGroup.value.accountId;
+    let accountId : string = this.accountFormGroup.value.accountId;
     this.account$ = this.accountService.getAccount(accountId, this.cuurentPage, this.pageSize);
   }
 
   gotoPage(page: number) {
     this.cuurentPage = page ;
     this.handleSearchAccount();
+  }
+
+  handleAccountOperation() {
+    let accountId : string = this.accountFormGroup.value.accountId;
+    let operationType : string = this.operationFormGroup.value.operationType;
+    let amount : number = this.operationFormGroup.value.amount;
+    let description : string = this.operationFormGroup.value.description;
+    let accountDestination : string = this.operationFormGroup.value.accountDestination;
+    if (operationType=='DEBIT'){
+      this.accountService.debit(accountId,amount,description).subscribe({
+        next : value => {
+          alert("Debit Operation has been successfully executed !");
+          this.operationFormGroup.reset();
+          this.handleSearchAccount();
+        }
+      });
+    } else if(operationType=='CREDIT'){
+      this.accountService.credit(accountId,amount,description).subscribe({
+        next : value => {
+          alert("Credit Operation has been successfully executed !");
+          this.operationFormGroup.reset();
+          this.handleSearchAccount();
+        }
+      });
+    } else if(operationType=='TRANSFERT'){
+      this.accountService.transfert(accountId, accountDestination,amount,description).subscribe({
+        next : value => {
+          alert("Transfert Operation has been successfully executed !");
+          this.operationFormGroup.reset();
+          this.handleSearchAccount();
+        }
+      });
+    }
   }
 }
--- src/app/services/accounts/accounts.service.ts
+++ src/app/services/accounts/accounts.service.ts
@@ -11,7 +11,22 @@ export class AccountsService {
   backenHost : String = "http://localhost:8082";
   constructor(private http : HttpClient) { }
 
-  public getAccount(accountId : String, page : number, size : number) : Observable<AccountDetail>{
+  public getAccount(accountId : string, page : number, size : number) : Observable<AccountDetail>{
     return this.http.get<AccountDetail>(this.backenHost+"/accounts/"+accountId+"/pageOperations?page="+page+"&size="+size);
+  }
+
+  public debit(accountId : String, amount : number, description : string){
+    let data = {accountId, amount, description};
+    return this.http.post(this.backenHost+"/accounts/debit",data);
+  }
+
+  public credit(accountId : String, amount : number, description : string){
+    let data = {accountId, amount, description};
+    return this.http.post(this.backenHost+"/accounts/credit",data);
+  }
+
+  public transfert(accountSource : String, accountDestination : String, amount : number, description : string){
+    let data = {accountSource, accountDestination, amount, description};
+    return this.http.post(this.backenHost+"/accounts/transfert",data);
   }
 }
